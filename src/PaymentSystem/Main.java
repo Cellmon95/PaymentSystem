12a235156cb3082d1bd7a777e2cbb1c4801c23e4
Lucas Källberg <lucas.arne@hotmail.com>
2025-09-24 16:53:09 +0200

    worked op payment

--- src/PaymentSystem/Main.java
+++ src/PaymentSystem/Main.java
@@ -1,7 +1,12 @@
+package PaymentSystem;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Cashier cashier = new Cashier();
+
 
     }
 }
